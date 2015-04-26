33835ec4767d6a21541e4776b67f231cea1a4995
Stephan Hochhaus <stephan@yauh.de>
2015-04-26 11:23:37 +0200

    Listing 6.6 Sending verification emails upon user creation

--- userApp/server/server.js
+++ userApp/server/server.js
@@ -5,5 +5,10 @@ Accounts.onCreateUser(function (options, user) {
     user.profile = {};
   }
   user.profile.rank = "White belt";
+  if (options.email) {
+    Meteor.setTimeout(function () {
+      Accounts.sendVerificationEmail(user._id);
+    }, 2 * 1000);
+  }
   return user;
 });
