59552f7ad6040edbdcbf27f0fdbf72805599ee41
Stephan Hochhaus <stephan@yauh.de>
2015-04-26 12:06:19 +0200

    Listing 6.10 Adding Facebook information to the user profile

--- userApp/server/server.js
+++ userApp/server/server.js
@@ -21,5 +21,10 @@ Accounts.onCreateUser(function (options, user) {
       Accounts.sendVerificationEmail(user._id);
     }, 2 * 1000);
   }
+  if (user.services.facebook) {
+    user.profile.first_name = user.services.facebook.first_name;
+    user.profile.last_name = user.services.facebook.last_name;
+    user.profile.gender = user.services.facebook.gender;
+  }
   return user;
 });
