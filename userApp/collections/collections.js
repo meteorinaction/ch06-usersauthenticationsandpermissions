570494f7ae81d70ffb2c936f06eda371ba8d3cfa
Stephan Hochhaus <stephan@yauh.de>
2015-04-26 12:12:46 +0200

    Listing 6.11 Using allow for a Messages collection

---
+++ userApp/collections/collections.js
@@ -0,0 +1,12 @@
+MessagesCollection = new Mongo.Collection('messages');
+MessagesCollection.allow({
+  insert: function (userId, doc) {
+    return true;
+  },
+  update: function (userId, doc) {
+    return true;
+  },
+  remove: function (userId, doc) {
+    return true;
+  }
+});
